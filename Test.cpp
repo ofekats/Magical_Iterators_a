3496a82e88ee03de232cd65131c6ea407755a162
ofekats <ofekats@gmail.com>
2023-06-01 00:51:19 +0300

    4

--- Test.cpp
+++ Test.cpp
@@ -136,7 +136,88 @@ TEST_CASE("AscendingIterator - operator>")
     //check if they throw
     CHECK_NOTHROW(asc_iter1.operator>(asc_iter2));
     CHECK(asc_iter1.operator>(asc_iter2) == false);
+    ++asc_iter1;
+    CHECK(asc_iter1.operator>(asc_iter2));
     CHECK_THROWS(asc_iter1.operator>(asc_iter3));
     CHECK_THROWS(asc_iter3.operator>(asc_iter2));
     CHECK_NOTHROW(asc_iter3.operator>(asc_iter3));
+}
+
+TEST_CASE("AscendingIterator - operator<")
+{
+    //create 2 MagicalContainer
+    MagicalContainer container1;
+    MagicalContainer container2;
+    //add elements
+    container1.addElement(-17);
+    container2.addElement(-17);
+    //create 3 AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container1);
+    MagicalContainer::AscendingIterator asc_iter2(container1);
+    MagicalContainer::AscendingIterator asc_iter3(container2);
+    //check if they throw
+    CHECK_NOTHROW(asc_iter1.operator>(asc_iter2));
+    CHECK(asc_iter1.operator<(asc_iter2) == false);
+    ++asc_iter2;
+    CHECK(asc_iter1.operator<(asc_iter2));
+    CHECK_THROWS(asc_iter1.operator<(asc_iter3));
+    CHECK_THROWS(asc_iter3.operator<(asc_iter2));
+    CHECK_NOTHROW(asc_iter3.operator<(asc_iter3));
+}
+
+TEST_CASE("AscendingIterator - Dereference operator (operator*)")
+{
+    //create MagicalContainer
+    MagicalContainer container;
+    //add elements
+    container.addElement(-17);
+    container.addElement(17);
+    //create AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container);
+    //check if they throw
+    CHECK(*asc_iter1 == -17);
+    ++asc_iter1;
+    CHECK(*asc_iter1 == 17);
+}
+
+TEST_CASE("AscendingIterator - Pre-increment operator (operator++)")
+{
+    //create MagicalContainer
+    MagicalContainer container;
+    //add elements
+    container.addElement(-17);
+    container.addElement(17);
+    //create AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container);
+    //check if they throw
+    CHECK_NOTHROW(++asc_iter1);
+    CHECK_THROWS(++asc_iter1);
+}
+
+TEST_CASE("AscendingIterator - begin")
+{
+    //create MagicalContainer
+    MagicalContainer container;
+    //add elements
+    container.addElement(17);
+    container.addElement(-17);
+    //create AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container);
+    //check if they throw
+    CHECK_NOTHROW(asc_iter1.begin());
+    CHECK(*asc_iter1.begin() == -17);
+}
+
+TEST_CASE("AscendingIterator - end")
+{
+    //create MagicalContainer
+    MagicalContainer container;
+    //add elements
+    container.addElement(17);
+    container.addElement(-17);
+    //create AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container);
+    //check if they throw
+    CHECK_NOTHROW(asc_iter1.end());
+    CHECK(*asc_iter1.end() == 17);
 }
