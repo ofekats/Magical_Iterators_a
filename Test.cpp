b1369a6f669a1e72fbd9372413ff9a230c090c5c
ofekats <ofekats@gmail.com>
2023-06-01 00:37:08 +0300

    3

--- Test.cpp
+++ Test.cpp
@@ -27,9 +27,9 @@ TEST_CASE("MagicalContainer - removeElement")
     //remove elements
     CHECK_NOTHROW(container.removeElement(-17));
     CHECK_NOTHROW(container.removeElement(0));
-    //remove element that doesnt exist
-    CHECK_THROWS(container.removeElement(-17));
-    CHECK_THROWS(container.removeElement(7));
+    //remove element that doesnt exist- should do nothing
+    CHECK_NOTHROW(container.removeElement(-17));
+    CHECK_NOTHROW(container.removeElement(7));
 }
 
 TEST_CASE("MagicalContainer - size")
@@ -61,6 +61,7 @@ TEST_CASE("AscendingIterator - constructors")
     //add elements
     container.addElement(-17);
     container.addElement(0);
+    //create with default, regular and copy costructors
     CHECK_NOTHROW(MagicalContainer::AscendingIterator());
     CHECK_NOTHROW(MagicalContainer::AscendingIterator asc_iter1(container));
     MagicalContainer::AscendingIterator asc_iter1(container);
@@ -68,4 +69,74 @@ TEST_CASE("AscendingIterator - constructors")
     MagicalContainer::AscendingIterator asc_iter2(asc_iter1);
     CHECK(asc_iter1 == asc_iter2);
 }
-//l
+
+TEST_CASE("AscendingIterator - Assignment operator")
+{
+    //create MagicalContainer
+    MagicalContainer container;
+    //add elements
+    container.addElement(-17);
+    container.addElement(0);
+    //create AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container);
+    //use Assignment operator
+    CHECK_NOTHROW(MagicalContainer::AscendingIterator asc_iter2 = asc_iter1);
+    MagicalContainer::AscendingIterator asc_iter2 = asc_iter1;
+    //should be equal
+    CHECK(asc_iter1 == asc_iter2);
+}
+
+TEST_CASE("AscendingIterator - Equality comparison (operator==)")
+{
+    //create 2 MagicalContainer
+    MagicalContainer container1;
+    MagicalContainer container2;
+    //add elements
+    container1.addElement(-17);
+    container2.addElement(0);
+    //create 3 AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container1);
+    MagicalContainer::AscendingIterator asc_iter2(asc_iter1);
+    MagicalContainer::AscendingIterator asc_iter3(container2);
+    //check if they equal
+    CHECK(asc_iter1.operator==(asc_iter2));
+    CHECK(asc_iter1.operator==(asc_iter3) == false);
+}
+
+
+TEST_CASE("AscendingIterator - Inequality comparison (operator!=)")
+{
+    //create 2 MagicalContainer
+    MagicalContainer container1;
+    MagicalContainer container2;
+    //add elements
+    container1.addElement(-17);
+    container2.addElement(0);
+    //create 3 AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container1);
+    MagicalContainer::AscendingIterator asc_iter2(asc_iter1);
+    MagicalContainer::AscendingIterator asc_iter3(container2);
+    //check if they not equal
+    CHECK(asc_iter1.operator!=(asc_iter2) == false);
+    CHECK(asc_iter1.operator!=(asc_iter3));
+}
+
+TEST_CASE("AscendingIterator - operator>")
+{
+    //create 2 MagicalContainer
+    MagicalContainer container1;
+    MagicalContainer container2;
+    //add elements
+    container1.addElement(-17);
+    container2.addElement(-17);
+    //create 3 AscendingIterator
+    MagicalContainer::AscendingIterator asc_iter1(container1);
+    MagicalContainer::AscendingIterator asc_iter2(container1);
+    MagicalContainer::AscendingIterator asc_iter3(container2);
+    //check if they throw
+    CHECK_NOTHROW(asc_iter1.operator>(asc_iter2));
+    CHECK(asc_iter1.operator>(asc_iter2) == false);
+    CHECK_THROWS(asc_iter1.operator>(asc_iter3));
+    CHECK_THROWS(asc_iter3.operator>(asc_iter2));
+    CHECK_NOTHROW(asc_iter3.operator>(asc_iter3));
+}
